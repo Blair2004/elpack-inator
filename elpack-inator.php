a9704393f6d491fbf4781cfbcfdb6ea4296cd68c
Developper <carlosjohnsonluv2004@gmail.com>
2020-08-30 23:39:31 +0100

    Update

---
+++ elpack-inator.php
@@ -0,0 +1,13 @@
+/**
+ * Plugin Name:       ElPack Inator
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       ElPack is a set of widgets and controls that are made to improve experience on Elementor.
+ * Version:           1.0.0
+ * Requires at least: 5.5
+ * Requires PHP:      7.2
+ * Author:            CodeWatchers
+ * Author URI:        https://codewatchers.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       elpack
+ * Domain Path:       /languages
